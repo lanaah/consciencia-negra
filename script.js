293bc7f01da0d1c0d83a95db5f190644425ef1b9
Alana <alanakarolinesouzacosta@gmail.com>
2023-11-14 01:02:04 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,57 @@
+function showSubMenu() {
+    document.getElementById('submenu').style.display = 'block';
+}
+
+function hideSubMenu() {
+    document.getElementById('submenu').style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('quem-somos-link').addEventListener('click', function () {
+        document.getElementById('sessao6').scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuLinks = document.querySelectorAll('#menu a');
+
+    menuLinks.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const sessaoAlvo = link.getAttribute('data-sessao');
+            document.getElementById(sessaoAlvo).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+
+    const quemSomosLink = document.getElementById('quem-somos-link');
+
+    quemSomosLink.addEventListener('click', function () {
+        document.getElementById('sessao6').scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+function openLink(link) {
+    window.open(link, '_blank');
+}
+
+function addHoverEffect(element) {
+    element.querySelector('.hover-overlay').style.display = 'block';
+}
+
+function removeHoverEffect(element) {
+    element.querySelector('.hover-overlay').style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        var rodape = document.getElementById('rodape');
+        var altura = document.documentElement.scrollHeight - window.innerHeight;
+        var scrollAtual = window.scrollY;
+
+        if (scrollAtual >= altura) {
+            rodape.classList.add('visivel');
+        } else {
+            rodape.classList.remove('visivel');
+        }
+    });
+});
